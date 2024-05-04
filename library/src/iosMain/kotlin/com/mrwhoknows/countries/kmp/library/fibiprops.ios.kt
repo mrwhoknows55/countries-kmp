597de6f575dae6fe28b4a6838f97baa194401e2f
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2024-05-05 00:45:31 +0530

    chore: make template changes for group id, packages setup

--- library/src/iosMain/kotlin/com/mrwhoknows/countries/kmp/library/fibiprops.ios.kt
+++ library/src/iosMain/kotlin/com/mrwhoknows/countries/kmp/library/fibiprops.ios.kt
@@ -1,2 +1,4 @@
+package com.mrwhoknows.countries.kmp.library
+
 actual val firstElement: Int = 3
 actual val secondElement: Int = 4
--- library/src/jvmMain/kotlin/com/mrwhoknows/countries/kmp/library/fibiprops.jvm.kt
+++ library/src/jvmMain/kotlin/com/mrwhoknows/countries/kmp/library/fibiprops.jvm.kt
@@ -1,2 +1,4 @@
+package com.mrwhoknows.countries.kmp.library
+
 actual val firstElement: Int = 2
 actual val secondElement: Int = 3
